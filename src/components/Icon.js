60ebb92d6924f551a38059692121abac24cba27f
JoaoGracindo <gabrielpessoa97@gmail.com.com>
2022-07-25 13:23:05 -0300

    aswer state and lock aswer

--- src/components/Icon.js
+++ src/components/Icon.js
@@ -1,6 +1,4 @@
 export default function Icon (props){
-    console.log(props)
-    console.log(props.icon)
     return (
         <ion-icon name={props.icon}></ion-icon>
     )
--- src/components/Question/Question.js
+++ src/components/Question/Question.js
@@ -1,28 +1,51 @@
+import react from "react";
 import React from "react";
 import Icon from "../Icon";
 import './style.css';
 
-export default function Question ({question, answer, key}){
+export default function Question ({question, answer, index}){
     const [turned, setTurned] = React.useState(false)
+    const [alreadyAnswered, setAlreadyAnswered] = React.useState(false)
+    const [reveled, setReveled] = React.useState(false)
+    function respondido (){
+        setReveled(!reveled);
+        setTurned(!turned);
+        setAlreadyAnswered(!alreadyAnswered);
+    }
     return(
         <div>
             <div className={turned ? 'hidden' : 'front'}>
                 <div>
-                    Pergunta {parseInt(key) + 1}
+                    Pergunta {index + 1}
                 </div>
-                <div onClick={() => setTurned(true)}>
+                <div onClick={() => {if(!alreadyAnswered){setTurned(!turned)}}}>
                     <Icon icon="play-outline"/>
                 </div>
             </div>
-            <div className={turned ? 'question' : 'hidden'}>
+            <div className={(turned  && !reveled)? 'question' : 'hidden'}>
                 <div>
                     {question}
                 </div>
-                <div>
+                <div onClick={() => setReveled(!reveled)}>
                     <Icon icon="reload-outline" />
+                </div>
+
+            </div>
+            <div className={reveled ? 'answer' : 'hidden'}>
+                <div>
+                    {answer}
+                </div>
+                <div>
+                    <button onClick={respondido}>Não lembrei</button>
+                    <button onClick={respondido}>Quase não lembrei</button>
+                    <button onClick={respondido}>Zap!</button>
                 </div>
 
             </div>
         </div>
     )
 }
+
+{/* <Card />
+<Questao />
+<Respuesta /> */}
